7eb1c226f7e817330184e541d1a57f1821292dee
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2018-06-21 22:12:12 +0300

    feature :Fetch articles from news api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import React, { PureComponent, Fragment } from "react";
+import MainComponent from "./layouts/MainComponent";
+import HeaderComponent from "./layouts/HeaderComponent";
+import Sidebar from "./layouts/Sidebar";
+
+class App extends PureComponent {
+    render() {
+        return (
+            <Fragment>
+                <HeaderComponent />
+                <div className="app columns">
+                    <Sidebar />
+                    <MainComponent />
+                </div>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
--- src/layouts/HeaderComponent.js
+++ src/layouts/HeaderComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import IconNews from "../icons/IconNews";
 import FiltersComponent from "../components/FiltersComponent";
+import IconSearch from "../icons/IconSearch";
 export default class HeaderComponent extends Component {
     render() {
         return (
-            <header className="main-header d-flex aic">
+            <header className="main-header d-flex aic jcsb">
                 <a href="" className="header-brand d-flex aic column-2">
                     <i className="header-icon">
                         <IconNews />
@@ -12,6 +13,11 @@ export default class HeaderComponent extends Component {
                     NF
                 </a>
                 <FiltersComponent />
+                <div className="mobile-search">
+                    <button className="btn btn-primary">
+                        <IconSearch />
+                    </button>
+                </div>
             </header>
         );
     }
